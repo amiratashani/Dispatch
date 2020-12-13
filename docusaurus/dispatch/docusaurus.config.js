941e4e728ed7b5f7394e333bb856b36d27371f2a
Rick Busarow <rickbusarow@gmail.com>
2020-12-13 12:55:45 -0600

    WIP

---
+++ docusaurus/dispatch/docusaurus.config.js
@@ -0,0 +1,101 @@
+module.exports = {
+  title: 'Dispatch',
+  tagline: 'Quality of life coroutine utilities',
+  url: 'https://rbusarow.github.io/Dispatch/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'rbusarow', // Usually your GitHub org/user name.
+  projectName: 'Dispatch', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/rbusarow/Dispatch',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Change Log',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/dispatch',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/rbusarow',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/rbusarow/Dispatch/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Rick Busarow, Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/rbusarow/Dispatch/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/rbusarow/Dispatch/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ docusaurus/dispatch/sidebars.js
@@ -0,0 +1,15 @@
+module.exports = {
+  someSidebar: {
+  Modules: ['dispatch-android-espresso',
+  'dispatch-android-lifecycle',
+  'dispatch-android-lifecycle-extensions',
+  'dispatch-android-viewmodel',
+  'dispatch-core',
+  'dispatch-detekt',
+  'dispatch-test',
+  'dispatch-test-junit4',
+  'dispatch-test-junit5'],
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'],
+  },
+};
